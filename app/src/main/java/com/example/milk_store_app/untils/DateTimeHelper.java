1cc3332e2345d8c73701d865a2b5602e76b114e7
lqviet45 <lqviet455@gmail.com>
2024-10-22 22:34:54 +0700

    adding order staff view

--- app/src/main/java/com/example/milk_store_app/untils/DateTimeHelper.java
+++ app/src/main/java/com/example/milk_store_app/untils/DateTimeHelper.java
@@ -6,13 +6,26 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeHelper {
+    /**
+     * Parse a string to LocalDateTime
+     * @param dateString the string to parse
+     * @return the LocalDateTime object
+     */
     public static LocalDateTime parseStringToLocalDateTime(String dateString) {
         DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
         return LocalDateTime.parse(dateString, formatter);
     }
 
+    /**
+     * Format a LocalDateTime object to a string with the format "yyyy-MM-dd HH:mm:ss"
+     * <p>
+     *     example: "2021-09-01 12:00:00"
+     * </p>
+     * @param localDateTime the LocalDateTime object to format
+     * @return the formatted string
+     */
     public static String formatLocalDateTimeToString(@NonNull LocalDateTime localDateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return localDateTime.format(formatter);
     }
 }
